afd68bc39903b1ae5220aeb1d6f615001924a324
TMad <madsen.tyler@gmail.com>
2018-02-04 20:45:35 -0700

    build session create action for linkedin login

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,7 +1,6 @@
 class SessionsController < ApplicationController
 
   def create
-    require "pry"; binding.pry
     if user = User.from_omniauth(request.env["omniauth.auth"])
       session[:user_id] = user.id
     end
