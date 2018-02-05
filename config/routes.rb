5678adfeca431bddd9c6072bd94e8efbbbb79ea1
TMad <madsen.tyler@gmail.com>
2018-02-04 17:47:50 -0700

    add omniauth routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   root 'home#index'
 
-  get 'auth/linkedin', as: :linked_login
+  get 'auth/linkedin', as: :linkedin_login
 
   get '/auth/linkedin/callback', to: 'sessions#create'
   delete '/logout', as: :logout, to: 'sessions#destroy'
