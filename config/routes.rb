427abd6001b88fab082b25648b917bd05b83a9a1
TMad <madsen.tyler@gmail.com>
2018-02-04 17:23:45 -0700

    add oauth routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,10 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  root 'home#index'
+
+  get 'auth/linkedin', as: :linked_login
+
+  get '/auth/linkedin/callback', to: 'sessions#create'
+  delete '/logout', as: :logout, to: 'sessions#destroy'
+
+  resources :dashboard, only: [:index]
 end
