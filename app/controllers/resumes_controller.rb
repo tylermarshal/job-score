921c4ac47db3507261f7387cab3f7a028118734b
TMad <madsen.tyler@gmail.com>
2018-02-21 22:16:13 -0700

    add resume controller with create action

---
+++ app/controllers/resumes_controller.rb
@@ -0,0 +1,19 @@
+class ResumesController < ApplicationController
+
+  def create
+    @resume = current_user.resumes.new(resume_params)
+    if @resume.save!
+      redirect_to dashboard_index_path
+    else
+      flash.notice = "Something went wrong, try adding your resume again."
+      redirect_to dashboard_index_path
+    end
+  end
+
+  private
+
+    def resume_params
+      params.require(:resume).permit(:name, :body)
+    end
+
+end
