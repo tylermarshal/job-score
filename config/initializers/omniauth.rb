2eafa131b7e1aa8b13dc0301acc9d556599adea3
TMad <madsen.tyler@gmail.com>
2018-02-23 12:35:34 -0700

    capitalize I in LinkedIn for omniauth initalization

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,3 +1,3 @@
 Rails.application.config.middleware.use OmniAuth::Builder do
-  provider :linkedin, ENV["LINKEDIN_CLIENT_ID"], ENV["LINKEDIN_CLIENT_SECRET"]
+  provider :linkedIn, ENV["LINKEDIN_CLIENT_ID"], ENV["LINKEDIN_CLIENT_SECRET"]
 end
