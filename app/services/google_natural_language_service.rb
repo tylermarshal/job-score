2285b9ccbca0f81629918801352edfd0f93ea54c
TMad <madsen.tyler@gmail.com>
2018-02-23 12:37:04 -0700

    add google natural language api service

---
+++ app/services/google_natural_language_service.rb
@@ -0,0 +1,19 @@
+require "google/cloud/language"
+class GoogleNaturalLanguageService
+
+  def analyze(text_content)
+    language = Google::Cloud::Language.new
+
+    response = language.analyze_entity_sentiment content: text_content, type: :PLAIN_TEXT
+
+    entities = response.entities
+
+    entities.each do |entity|
+      puts "Entity #{entity.name} #{entity.type}"
+
+      if entity.metadata["wikipedia_url"]
+        puts "URL: #{entity.metadata['wikipedia_url']}"
+      end
+    end
+  end
+end
