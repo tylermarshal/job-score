d8401c4e396700e28d13e0abb562794ffb17697c
TMad <madsen.tyler@gmail.com>
2018-02-23 12:33:57 -0700

    add cover letter create action & controller

---
+++ app/controllers/cover_letters_controller.rb
@@ -0,0 +1,19 @@
+class CoverLettersController < ApplicationController
+
+  def create
+    @cover_letter = current_user.cover_letters.new(cover_letter_params)
+    if @cover_letter.save!
+      redirect_to dashboard_index_path
+    else
+      flash.notice = "Something went wrong, try adding your cover letter again."
+      redirect_to dashboard_index_path
+    end
+  end
+
+  private
+
+    def cover_letter_params
+      params.require(:cover_letter).permit(:name, :body)
+    end
+
+end
