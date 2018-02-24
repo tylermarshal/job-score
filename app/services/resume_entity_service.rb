2109cac537efdf3e18c43e312242f93faf71bbe7
TMad <madsen.tyler@gmail.com>
2018-02-23 20:04:55 -0700

    add salience & magnitude != 0 conditional

--- app/services/resume_entity_service.rb
+++ app/services/resume_entity_service.rb
@@ -3,20 +3,22 @@ class ResumeEntityService
   def generate(resume)
     entities = GoogleNaturalLanguageService.new.analyze(resume.body)
     entities.each do |entity|
-      if entity.metadata.has_key?("wikipedia_url")
-        resume.resume_entities.create!(
-          entity: entity.name,
-          salience: entity.salience,
-          magnitude: entity.sentiment.magnitude,
-          wikipedia: entity.metadata["wikipedia_url"]
-        )
-      else
-        resume.resume_entities.create!(
-          entity: entity.name,
-          salience: entity.salience,
-          magnitude: entity.sentiment.magnitude,
-          wikipedia: nil
-        )
+      if entity.salience != 0.0 && entity.sentiment.magnitude != 0.0
+        if entity.metadata.has_key?("wikipedia_url")
+          resume.resume_entities.create!(
+            entity: entity.name,
+            salience: entity.salience,
+            magnitude: entity.sentiment.magnitude,
+            wikipedia: entity.metadata["wikipedia_url"]
+          )
+        else
+          resume.resume_entities.create!(
+            entity: entity.name,
+            salience: entity.salience,
+            magnitude: entity.sentiment.magnitude,
+            wikipedia: nil
+          )
+        end
       end
     end
   end
