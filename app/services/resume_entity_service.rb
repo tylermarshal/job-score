9f85a16f6954bdce8fc57ffb6e75e0800e2f819f
TMad <madsen.tyler@gmail.com>
2018-02-23 19:53:30 -0700

    add resume entity service spec

---
+++ app/services/resume_entity_service.rb
@@ -0,0 +1,24 @@
+class ResumeEntityService
+
+  def generate(resume)
+    entities = GoogleNaturalLanguageService.new.analyze(resume.body)
+    entities.each do |entity|
+      if entity.metadata.has_key?("wikipedia_url")
+        resume.resume_entities.create!(
+          entity: entity.name,
+          salience: entity.salience,
+          magnitude: entity.sentiment.magnitude,
+          wikipedia: entity.metadata["wikipedia_url"]
+        )
+      else
+        resume.resume_entities.create!(
+          entity: entity.name,
+          salience: entity.salience,
+          magnitude: entity.sentiment.magnitude,
+          wikipedia: nil
+        )
+      end
+    end
+  end
+
+end
