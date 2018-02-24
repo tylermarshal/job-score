243dc9f9025f329fe5a56786662b61bc7553f2d2
TMad <madsen.tyler@gmail.com>
2018-02-23 17:33:47 -0700

    add job controller with create action

---
+++ app/controllers/jobs_controller.rb
@@ -0,0 +1,19 @@
+class JobsController < ApplicationController
+
+  def create
+    @job = current_user.jobs.new(job_params)
+    if @job.save!
+      redirect_to dashboard_index_path
+    else
+      flash.notice = "Something went wrong, try adding the job description again."
+      redirect_to dashboard_index_path
+    end
+  end
+
+  private
+
+    def job_params
+      params.require(:job).permit(:company_name, :job_title, :description)
+    end
+
+end
