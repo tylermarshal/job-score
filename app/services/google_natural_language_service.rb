dee9772f0f9c4c23c45e9c324bc07a1604ee1c14
TMad <madsen.tyler@gmail.com>
2018-02-23 17:34:06 -0700

    comment out puts statements in google natural lang service

--- app/services/google_natural_language_service.rb
+++ app/services/google_natural_language_service.rb
@@ -6,14 +6,15 @@ class GoogleNaturalLanguageService
 
     response = language.analyze_entity_sentiment content: text_content, type: :PLAIN_TEXT
 
-    entities = response.entities
+    response.entities
+    # entities = response.entities
 
-    entities.each do |entity|
-      puts "Entity #{entity.name} #{entity.type}"
-
-      if entity.metadata["wikipedia_url"]
-        puts "URL: #{entity.metadata['wikipedia_url']}"
-      end
-    end
+    # entities.each do |entity|
+    #   puts "Entity #{entity.name} #{entity.type}"
+    #
+    #   if entity.metadata["wikipedia_url"]
+    #     puts "URL: #{entity.metadata['wikipedia_url']}"
+    #   end
+    # end
   end
 end
