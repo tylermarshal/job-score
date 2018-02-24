28e9450aab1ad963d62e2b45f83365dcc1d0a519
TMad <madsen.tyler@gmail.com>
2018-02-23 20:29:17 -0700

    comment out vcr in spec

--- spec/models/resume_entity_spec.rb
+++ spec/models/resume_entity_spec.rb
@@ -5,7 +5,7 @@ RSpec.describe ResumeEntity, type: :model do
     text = "As a customer-obsessed professional with a passion for the healthcare industry, I was thrilled to learn that Health First Medical Clinic needs a Customer Service Manager. Presently, I am a Senior Customer Service Representative with six years of experience. I am registered with the California State Board of Pharmacy and have a B.A. in Management from Golden Gate University in San Francisco."
     user = create(:user)
     resume = create(:resume, user: user, body: text)
-    VCR.use_cassette("services/find_resume_entities") do
+    # VCR.use_cassette("services/find_resume_entities") do
 
       entities = ResumeEntityService.new.generate(resume)
       entity = entities.first
@@ -14,6 +14,6 @@ RSpec.describe ResumeEntity, type: :model do
       expect(entity.name).to eq("Customer Service Representative")
       expect(entity.salience).to eq(0.5650523900985718)
       expect(entity.sentiment.magnitude).to eq(0.0)
-    end
+    # end
   end
 end
