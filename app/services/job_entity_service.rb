ec372c72d6c10da694a8f5dd2b8663e3707f1c1d
TMad <madsen.tyler@gmail.com>
2018-02-23 20:28:49 -0700

    add job entity service

---
+++ app/services/job_entity_service.rb
@@ -0,0 +1,26 @@
+class JobEntityService
+
+  def generate(job)
+    entities = GoogleNaturalLanguageService.new.analyze(job.body)
+    entities.each do |entity|
+      if entity.salience != 0.0 && entity.sentiment.magnitude != 0.0
+        if entity.metadata.has_key?("wikipedia_url")
+          job.job_entities.create!(
+            entity: entity.name,
+            salience: entity.salience,
+            magnitude: entity.sentiment.magnitude,
+            wikipedia: entity.metadata["wikipedia_url"]
+          )
+        else
+          job.job_entities.create!(
+            entity: entity.name,
+            salience: entity.salience,
+            magnitude: entity.sentiment.magnitude,
+            wikipedia: nil
+          )
+        end
+      end
+    end
+  end
+
+end
