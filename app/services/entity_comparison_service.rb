e76f54d9e155dcffc18049fc880e089884482f31
TMad <madsen.tyler@gmail.com>
2018-02-26 16:01:22 -0700

    add percentage method to comparison service

--- app/services/entity_comparison_service.rb
+++ app/services/entity_comparison_service.rb
@@ -7,6 +7,13 @@ class EntityComparisonService
     end
   end
 
-  
+  def self.percentage(job, document)
+    job.job_entities.reduce(0) do |result, word|
+      result += 1 if job.company_name == word.entity || document.body.include?(word.entity)
+      result
+    end.to_f/job.job_entities.count
+  end
+
+
 
 end
