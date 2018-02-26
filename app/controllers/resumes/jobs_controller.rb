ed8a8cfb7a038a8cb2c1338990a146cc417bc499
TMad <madsen.tyler@gmail.com>
2018-02-26 14:54:29 -0700

    send all current user jobs and resumes to the view

--- app/controllers/resumes/jobs_controller.rb
+++ app/controllers/resumes/jobs_controller.rb
@@ -1,9 +1,11 @@
 class Resumes::JobsController < ApplicationController
 
   def show
-    @job_entities = current_user.jobs.find(params[:id]).job_entities
-    @resume_entities = current_user.resumes.find(params[:resume]).resume_entities
-    @keywords = EntityComparisonService.compare(@job_entities, @resume_entities)
+    @user_jobs = current_user.jobs
+    @user_resumes = current_user.resumes
+    @job = current_user.jobs.find(params[:id])
+    @resume = current_user.resumes.find(params[:resume])
+    @keywords = EntityComparisonService.compare(@job, @resume)
   end
 
 end
