e1816f546d8033d11d7a7a79e73be15615bda14a
TMad <madsen.tyler@gmail.com>
2018-02-26 14:56:23 -0700

    refactor compar method

--- app/services/entity_comparison_service.rb
+++ app/services/entity_comparison_service.rb
@@ -1,17 +1,12 @@
 class EntityComparisonService
 
-  def self.compare(job_entities, document_entities)
-    # job_entities.order("magnitude DESC, salience DESC")
-    # document_entities.order("magnitude DESC, salience DESC")
-    job = job_entities.sort_by do |entity|
-      entity.magnitude * entity.salience
-    end.reverse
-    document = document_entities.sort_by do |entity|
-      entity.magnitude * entity.salience
-    end.reverse
-    job_words = job.map do |word|
-      word.entity
+  def self.compare(job, document)
+    ordered_job_entities = job.job_entities.order(multiplier: :desc)
+    ordered_job_entities.reject do |word|
+      job.company_name == word.entity || document.body.include?(word.entity)
     end
   end
+
+  
 
 end
