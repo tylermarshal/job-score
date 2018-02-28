c1f248a1a7b59af2d2f602d46b9e9d6e7c8c106d
TMad <madsen.tyler@gmail.com>
2018-02-28 15:57:08 -0700

    add methods by tone for cover letter sentence tone model

--- app/models/cover_letter_sentence_tone.rb
+++ app/models/cover_letter_sentence_tone.rb
@@ -1,3 +1,31 @@
 class CoverLetterSentenceTone < ApplicationRecord
   belongs_to :cover_letter
+
+  def self.confident
+    where(tone: "Confident")
+  end
+
+  def self.fear
+    where(tone: "Fear")
+  end
+
+  def self.anger
+    where(tone: "Anger")
+  end
+
+  def self.joy
+    where(tone: "Joy")
+  end
+
+  def self.sadness
+    where(tone: "Sadness")
+  end
+
+  def self.analytical
+    where(tone: "Analytical")
+  end
+
+  def self.tentative
+    where(tone: "Tentative")
+  end
 end
