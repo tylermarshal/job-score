63d4ba13eb0cd6625bc269ec4b4ccb3bf57dd0ed
TMad <madsen.tyler@gmail.com>
2018-02-28 13:07:20 -0700

    add tone analyzer service

---
+++ app/services/tone_analyzer_service.rb
@@ -0,0 +1,36 @@
+class ToneAnalyzerService
+
+  attr_reader :response, :cover_letter
+
+  def initialize(cover_letter)
+    @cover_letter = cover_letter
+    @response = WatsonService.new(cover_letter.body).parse
+  end
+
+  def generate
+    generate_document_tone
+    generate_sentence_tone
+  end
+
+  def generate_document_tone
+    response[:document_tone][:tones].each do |tone|
+      cover_letter.cover_letter_document_tones.create!(
+        tone: tone[:tone_name],
+        score: tone[:score]
+      )
+    end
+  end
+
+  def generate_sentence_tone
+    response[:sentences_tone].each do |sentence|
+      sentence[:tones].each do |tone|
+        cover_letter.cover_letter_sentence_tones.create!(
+          sentence: sentence[:text],
+          tone: tone[:tone_name],
+          score: tone[:score]
+        )
+      end
+    end
+  end
+
+end
