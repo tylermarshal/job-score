e09748bb5b61e261c9b6b15a522fce6a24a0b9ec
TMad <madsen.tyler@gmail.com>
2018-02-28 15:56:51 -0700

    fix cover letter analysis controller conditional

--- app/controllers/cover_letter_analysis_controller.rb
+++ app/controllers/cover_letter_analysis_controller.rb
@@ -3,7 +3,7 @@ class CoverLetterAnalysisController < ApplicationController
   def show
     @user_jobs = current_user.jobs
     @user_cover_letters = current_user.cover_letters
-    if params[:id] && params[:cover_letters]
+    if params[:id] && params[:cover_letter]
       @job = current_user.jobs.find(params[:id])
       @cover_letter = current_user.cover_letters.find(params[:cover_letter])
     else
