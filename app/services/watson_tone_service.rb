b52f0bd46d7a024c94884360602b5a2351cad295
TMad <madsen.tyler@gmail.com>
2018-02-27 19:52:45 -0700

    add watson tone analysis service

---
+++ app/services/watson_tone_service.rb
@@ -0,0 +1,24 @@
+class WatsonToneService
+
+  attr_reader :text
+
+  def initialize(text)
+    @text = text
+  end
+
+  def conn
+    Faraday.new(:url => 'https://gateway.watsonplatform.net') do |faraday|
+      faraday.basic_auth(ENV["WATSON_USERNAME"], ENV["WATSON_PASSWORD"])
+      faraday.adapter  Faraday.default_adapter
+    end
+  end
+
+  def response
+    conn.get("/tone-analyzer/api/v3/tone?version=2017-09-21&text=#{text}")
+  end
+
+  def parse
+    JSON.parse(response.body)
+  end
+
+end
