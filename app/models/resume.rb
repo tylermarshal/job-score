0bd2392c494c42f188f8bc6612ae365f9f25ca3e
TMad <madsen.tyler@gmail.com>
2018-02-27 19:51:32 -0700

    add word count before save to resume

--- app/models/resume.rb
+++ app/models/resume.rb
@@ -1,4 +1,10 @@
 class Resume < ApplicationRecord
   belongs_to :user
   has_many :resume_entities, dependent: :destroy
+
+  before_save :calculate_word_count
+
+  def calculate_word_count
+    self.word_count = body.split(" ").count
+  end
 end
