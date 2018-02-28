84972de1d4fa5ef915c38a8153da4406149f0296
TMad <madsen.tyler@gmail.com>
2018-02-28 14:46:57 -0700

    add conditional for cover letter sentiment service

--- app/services/cover_letter_sentiment_service.rb
+++ app/services/cover_letter_sentiment_service.rb
@@ -3,11 +3,12 @@ class CoverLetterSentimentService
   def self.generate(cover_letter)
     sentences = GoogleSentimentService.analyze(cover_letter.body)
     sentences.each do |entity|
-      cover_letter.cover_letter_sentiments.create!(
-        entity: entity.name,
-        salience: entity.salience,
-        magnitude: entity.sentiment.magnitude,
-        score: entity.sentiment.score)
+      if entity.sentiment.magnitude * entity.sentiment.score <= 0.02 && entity.sentiment.magnitude * entity.sentiment.score != 0.0
+        cover_letter.cover_letter_sentiments.create!(
+          sentence: entity.text.content,
+          magnitude: entity.sentiment.magnitude,
+          score: entity.sentiment.score)
+      end
     end
   end
 
