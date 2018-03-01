083a81c67f85b2bf7aa5c5bbb3d70976bdcb966c
TMad <madsen.tyler@gmail.com>
2018-02-28 21:46:11 -0700

    add destroy routes

--- config/routes.rb
+++ config/routes.rb
@@ -8,9 +8,9 @@ Rails.application.routes.draw do
 
   resources :dashboard, only: [:index]
 
-  resources :resumes, only: [:create]
-  resources :cover_letters, only: [:create]
-  resources :jobs, only: [:create]
+  resources :resumes, only: [:create, :destroy]
+  resources :cover_letters, only: [:create, :destroy]
+  resources :jobs, only: [:create, :destroy]
 
   get "/resume-analysis", to: "resume_analysis#show"
   get "/cover-letter-analysis", to: "cover_letter_analysis#show"
