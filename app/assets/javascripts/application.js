5e7d6bdf61b14c8596b522a8e00bbd06959c8623
TMad <madsen.tyler@gmail.com>
2018-02-28 19:30:50 -0700

    add chart setup files

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -10,7 +10,7 @@
 // Read Sprockets README (https://github.com/rails/sprockets#sprockets-directives) for details
 // about supported directives.
 //
-//= require Chart.bundle
+//= require highcharts
 //= require chartkick
 //= require rails-ujs
 //= require jquery
