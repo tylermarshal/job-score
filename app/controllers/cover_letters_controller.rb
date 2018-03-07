9d643d751199a4ef84c8ca83bc9926b2a8c1902d
TMad <madsen.tyler@gmail.com>
2018-03-07 16:55:40 -0700

    remove cover letter entity and sentiment service creation from cover letter controller

--- app/controllers/cover_letters_controller.rb
+++ app/controllers/cover_letters_controller.rb
@@ -3,8 +3,6 @@ class CoverLettersController < ApplicationController
   def create
     @cover_letter = current_user.cover_letters.new(cover_letter_params)
     if @cover_letter.save!
-      CoverLetterEntityService.generate(@cover_letter)
-      CoverLetterSentimentService.generate(@cover_letter)
       ToneAnalyzerService.new(@cover_letter).generate
       redirect_to dashboard_index_path
     else
