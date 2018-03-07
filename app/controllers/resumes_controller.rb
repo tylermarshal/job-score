c3af5ccb30d31528cf0834f41a257e855361bd63
TMad <madsen.tyler@gmail.com>
2018-03-07 16:55:59 -0700

    remove resume entity service creation from resume controller

--- app/controllers/resumes_controller.rb
+++ app/controllers/resumes_controller.rb
@@ -3,7 +3,6 @@ class ResumesController < ApplicationController
   def create
     @resume = current_user.resumes.new(resume_params)
     if @resume.save!
-      ResumeEntityService.new.generate(@resume)
       redirect_to dashboard_index_path
     else
       flash.notice = "Something went wrong, try adding your resume again."
