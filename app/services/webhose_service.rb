ba3c3e0586db5f161cc739df14bd48e5d06ae1ba
TMad <madsen.tyler@gmail.com>
2018-03-07 16:57:34 -0700

    add webhose service

---
+++ app/services/webhose_service.rb
@@ -0,0 +1,27 @@
+class WebhoseService
+
+  attr_reader :company
+
+  def initialize(company)
+    @company = URI::encode(company)
+  end
+
+  def conn
+    Faraday.new(:url => 'http://webhose.io') do |faraday|
+      faraday.adapter  Faraday.default_adapter
+    end
+  end
+
+  def response
+    conn.get("/filterWebContent?token=#{ENV["WEBHOSE_KEY"]}&format=json&sort=crawled&q=#{company}%20language%3Aenglish%20thread.country%3AUS")
+  end
+
+  def parse
+    JSON.parse(response.body, symbolize_names: true)
+  end
+
+  def generate
+    
+  end
+
+end
