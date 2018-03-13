3f79697bdde8ebcd39288d4456dfef2e48541a0e
TMad <madsen.tyler@gmail.com>
2018-03-13 17:35:48 -0600

    add webhose article relationship with job

--- app/models/job.rb
+++ app/models/job.rb
@@ -1,8 +1,16 @@
 class Job < ApplicationRecord
   belongs_to :user
   has_many :job_entities, dependent: :destroy
+  has_many :webhose_articles, dependent: :destroy
 
   def full_title
     "#{company_name}: #{job_title}"
+  end
+
+  def update_articles
+    if webhose_articles.first.created_at + 86400 <= Time.now
+      webhose_articles.destroy_all
+      WebhoseArticleBuilder.generate(self)
+    end
   end
 end
