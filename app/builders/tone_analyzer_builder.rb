31f4907e5095211bac6787c1287051702cb91616
TMad <madsen.tyler@gmail.com>
2018-03-13 20:49:52 -0600

    add unless conditional to tone analyzer builder

--- app/builders/tone_analyzer_builder.rb
+++ app/builders/tone_analyzer_builder.rb
@@ -13,22 +13,26 @@ class ToneAnalyzerBuilder
   end
 
   def generate_document_tone
-    response[:document_tone][:tones].each do |tone|
-      cover_letter.cover_letter_document_tones.create!(
-        tone: tone[:tone_name],
-        score: tone[:score]
-      )
+    unless !response[:document_tone]
+      response[:document_tone][:tones].each do |tone|
+        cover_letter.cover_letter_document_tones.create!(
+          tone: tone[:tone_name],
+          score: tone[:score]
+        )
+      end
     end
   end
 
   def generate_sentence_tone
-    response[:sentences_tone].each do |sentence|
-      sentence[:tones].each do |tone|
-        cover_letter.cover_letter_sentence_tones.create!(
-          sentence: sentence[:text],
-          tone: tone[:tone_name],
-          score: tone[:score]
-        )
+    unless !response[:sentences_tone]
+      response[:sentences_tone].each do |sentence|
+        sentence[:tones].each do |tone|
+          cover_letter.cover_letter_sentence_tones.create!(
+            sentence: sentence[:text],
+            tone: tone[:tone_name],
+            score: tone[:score]
+          )
+        end
       end
     end
   end
