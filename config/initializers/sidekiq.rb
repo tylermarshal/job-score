ca924ade3780ea2792d4f2402519d7b471a8498c
TMad <madsen.tyler@gmail.com>
2018-03-14 12:16:54 -0600

    add redis and sidekiq configs

--- config/initializers/sidekiq.rb
+++ config/initializers/sidekiq.rb
@@ -1,9 +1,9 @@
 require 'sidekiq'
 
 Sidekiq.configure_client do |config|
-  config.redis = { size: 1 }
+  config.redis = { url: ENV['REDISTOGO_URL'], size: 1 }
 end
 
 Sidekiq.configure_server do |config|
-  config.redis = { size: 4 }
+  config.redis = { url: ENV['REDISTOGO_URL'], size: 4 }
 end
