9ed6bd41c0d22eb49264d5a693bb3eda3be47de9
TMad <madsen.tyler@gmail.com>
2018-03-14 12:54:31 -0600

    add sidekiq config pool limits

--- config/initializers/sidekiq.rb
+++ config/initializers/sidekiq.rb
@@ -1,9 +1,9 @@
 require 'sidekiq'
 
 Sidekiq.configure_client do |config|
-  config.redis = { url: ENV['REDISTOGO_URL'], size: 5 }
+  config.redis = { url: ENV['REDISTOGO_URL'], size: 2 }
 end
 
 Sidekiq.configure_server do |config|
-  config.redis = { url: ENV['REDISTOGO_URL'], size: 22 }
+  config.redis = { url: ENV['REDISTOGO_URL'], size: 25 }
 end
