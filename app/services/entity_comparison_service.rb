8df7eec4b3b25634ad45940fe3cfe8b9c4f88579
TMad <madsen.tyler@gmail.com>
2018-03-14 21:11:17 -0600

    Order job entity alphabetically

--- app/services/entity_comparison_service.rb
+++ app/services/entity_comparison_service.rb
@@ -1,7 +1,7 @@
 class EntityComparisonService
 
   def self.compare(job, document)
-    ordered_job_entities = job.job_entities.order(multiplier: :desc).uniq {|entity| entity.entity}
+    ordered_job_entities = job.job_entities.order(:entity).uniq {|entity| entity.entity}
     ordered_job_entities.reject do |word|
       job.company_name == word.entity || document.body.include?(word.entity)
     end
