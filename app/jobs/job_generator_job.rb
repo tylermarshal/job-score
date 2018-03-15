0eb88c3b7f2048d33a9dc4729fd058057130f32b
TMad <madsen.tyler@gmail.com>
2018-03-14 20:57:54 -0600

    Remove remaining isntance variable from job generator job

--- app/jobs/job_generator_job.rb
+++ app/jobs/job_generator_job.rb
@@ -2,7 +2,7 @@ class JobGeneratorJob < ApplicationJob
   queue_as :default
 
   def perform(job)
-    JobEntityBuilder.generate(@job)
-    WebhoseArticleBuilder.generate(@job)
+    JobEntityBuilder.generate(job)
+    WebhoseArticleBuilder.generate(job)
   end
 end
